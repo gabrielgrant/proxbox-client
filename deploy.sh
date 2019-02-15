7ffd5ae9a560a76f5958ddabc48c9549f7a71587
Gabriel Grant <g@briel.ca>
2019-02-15 11:38:04 -0800

    add wait for houdini before trying to fetch cluster address

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 kubectl create -f https://raw.githubusercontent.com/gabrielgrant/proxbox-client/master/kubernetes-manifest.yaml
 
-
-#TODO wait until houdini is up
-
-
 POD_NAME=`kubectl get pod --output=json | jq -r '.items[] | select(.metadata.name|startswith("proxbox-client")).metadata.name'`  #  -r flag is needed to not get quotes in the output
+
+# wait until houdini is up
+until timeout 10s kubectl get pod $POD_NAME | grep "Running" ; do sleep 1; done;
+
 kubectl logs $POD_NAME | grep "Access your cluster at"
